698397b05bd8c71082b68720997470b25018d1fa
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-01 20:06:58 +0500

    Добавьте файлы проекта.

---
+++ HW_1.2_30-01-21/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW_1._2_30_01_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player action = new Player();
+            Console.WriteLine("Выди действый: ");
+            action.Play();
+            action.Record();
+            action.Pause();
+            action.Stop();
+        }
+    }
+    interface IPlayable
+    {
+        void Play(); void Pause(); void Stop();
+    }
+    interface IRecodable
+    {
+        void Record(); void Pause(); void Stop();
+    }
+    class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("play-------|>");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("pause------|>");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("stop-------|>");
+        }
+        public void Record()
+        {
+            Console.WriteLine("record-----|>");
+        }
+    }
+}   
